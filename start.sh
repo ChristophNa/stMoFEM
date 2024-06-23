982806cf7f7d1cc8c51fdf47f4a9fff1a8245ed9
naumannc <christoph.naumann1985@gmail.com>
2024-06-23 22:13:02 +0200

    added python3

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Run the background Python script
-python app/MoFEM/runSimulation.py &
+python3 app/MoFEM/runSimulation.py &
 
 # Run the Streamlit app
-streamlit run app/0_🏡_Home.py --server.port=8501 --server.address=0.0.0.0
+streamlit run app/0_🏡_Home.py --server.port=8501 --server.address=0.0.0.0 #> dev/null

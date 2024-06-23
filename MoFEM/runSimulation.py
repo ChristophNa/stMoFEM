1598088279f9886d78a44978158a04ccd6fa5e9d
naumannc <christoph.naumann1985@gmail.com>
2024-06-23 15:15:25 +0200

    removed bugs from not using attrDict

--- MoFEM/runSimulation.py
+++ MoFEM/runSimulation.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import subprocess
 import gmsh
 from convert import convert
+from time import sleep
 
 # generation of a config file - what attributes should the blocksets have
 def generate_config(params, path):
@@ -17,8 +18,8 @@ def generate_config(params, path):
                 # print the data as it is written to the file
                 print(line)
             # FORCE_1 boundary condition (change in params bellow)
-            data = ['[SET_ATTR_FORCE_1]', 'number_of_attributes=3', 'user1='+str(params.force_x), 
-                    'user2='+str(params.force_y), 'user3='+str(params.force_z)]
+            data = ['[SET_ATTR_FORCE_1]', 'number_of_attributes=3', 'user1='+str(params["force_x"]), 
+                    'user2='+str(params["force_y"]), 'user3='+str(params["force_z"])]
             # Use a for loop to write each line of data to the file
             for line in data:
                 f.write(line + '\n')
@@ -32,27 +33,27 @@ def generate_config(params, path):
 # gmsh creation of a 3D beam + visualisation of it
 def generate_mesh_box(params, path):
     try:
-        if (params.length_x+params.length_y+params.length_z)/params.element_size > 600:
+        if (params["length_x"]+params["length_y"]+params["length_z"])/params["element_size"] > 600:
             raise Exception("Not more than 200 elements per side should be used in this tutorial")
         
         # Initialize gmsh
         gmsh.initialize()
         gmsh.option.setNumber("General.Verbosity", 3)
 
-        box1 = gmsh.model.occ.add_box(0, 0, 0, params.length_x, params.length_y, params.length_z)
+        box1 = gmsh.model.occ.add_box(0, 0, 0, params["length_x"], params["length_y"], params["length_z"])
 
         # Create the relevant Gmsh data structures from Gmsh model.
         gmsh.model.occ.synchronize()
 
         # ensuring a structured mesh with required element size 
         for n in [0,2,4,6]:
-            N = int(params.length_z / params.element_size) + 1
+            N = int(params["length_z"] / params["element_size"]) + 1
             gmsh.model.mesh.setTransfiniteCurve(n+1, N,'Progression', 1.0)
         for n in [1,3,5,7]:
-            N = int(params.length_y / params.element_size) + 1
+            N = int(params["length_y"] / params["element_size"]) + 1
             gmsh.model.mesh.setTransfiniteCurve(n+1, N,'Progression', 1.0)
         for n in [8,9,10,11]:
-            N = int(params.length_x / params.element_size) + 1
+            N = int(params["length_x"] / params["element_size"]) + 1
             gmsh.model.mesh.setTransfiniteCurve(n+1, N,'Progression', 1.0)
 
         for n in range(len(gmsh.model.getEntities(2))):
@@ -69,7 +70,7 @@ def generate_mesh_box(params, path):
         gmsh.model.mesh.generate(3)
         
         # save as a .med file
-        gmsh.write(Path(path)/"mesh.med")
+        gmsh.write(str((Path(path)/"mesh.med").resolve()))
         
         # close gmsh
         gmsh.finalize()
@@ -96,7 +97,7 @@ def generate_mesh_box(params, path):
 
     #     p.camera_position = "xy"
     #     p.show(jupyter_backend='ipygany')
-    return
+    # return
 
 def parse_log_file(log_file):
     res = {}
@@ -115,43 +116,29 @@ def parse_log_file(log_file):
                 res.norm_error_u_l2 = float(line.split()[4])
     return res
 
-def createWorkDir(id, path):
-    try:
-        workDir = Path(path)/str(id)
-        workDir.mkdir(parents=True, exist_ok=True)
-        return True
-    except:
-        return False
-
-
-
 # running MoFEM executable
 def run_mofem(exe, params, path):
-    if params.order > 5:
-        raise Exception("Approximation order greater than 5 should not be used in this tutorial")
-    if params.nproc > 4:
-        raise Exception("Not more than 4 processors should be used in this tutorial")
-    
+
     # define name of the mesh file
-    part_file = params.mesh_file + f"_{params.nproc}p.h5m"
+    part_file = Path(path)/"mesh" + f'_{params["nproc"]}p.h5m'
     log_file = Path(path)/"MoFEM.log"
     
     # partition the mesh
-    subprocess.run(["mofem_part", "-my_file", Path(path)/"mesh" + ".h5m", "-my_nparts", str(params.nproc), "-output_file", part_file])
+    subprocess.run(["mofem_part", "-my_file", Path(path)/"mesh" + ".h5m", "-my_nparts", str(params["nproc"]), "-output_file", part_file])
     
     # choose MoFEM executable
     um_view = "$HOME/um_view"
     exes = {"Mixed": um_view + "/*eshelbian_*/ep", "Standard": um_view + "/tutorials/vec-2/nonlinear_elastic"}
     subprocess.run(["export", "OMPI_MCA_btl_vader_single_copy_mechanism=none", "&&",
                     "nice", "-n", "10", "mpirun", "--oversubscribe", "--allow-run-as-root",
-                    "-np", params.nproc, exes[params["formulation"]],
+                    "-np", params["nproc"], exes[params["formulation"]],
                     "-file_name", part_file, "-my_file", part_file,
-                    "-order", params.order,
-                    "-space_order", params.order + 1,
-                    "-hencky_young_modulus", params.young_modulus,
-                    "-hencky_poisson_ratio", params.poisson_ratio,
-                    "-rotations", params.rotations,
-                    "-stretches", params.stretches,
+                    "-order", params["order"],
+                    "-space_order", params["order"] + 1,
+                    "-hencky_young_modulus", params["young_modulus"],
+                    "-hencky_poisson_ratio", params["poisson_ratio"],
+                    "-rotations", params["rotations"],
+                    "-stretches", params["stretches"],
                     "2>&1", "|", "tee", log_file])
 
 
@@ -161,40 +148,59 @@ def run_mofem(exe, params, path):
 # The workflow
 if __name__=="__main__":
     # set the base path
-    basePath = Path("/tmp/simulations")
+    basePath = Path("simulations")
     
     while True:
         users = [item for item in basePath.iterdir() if item.is_dir()]
+        print(users)
         for user in users:
-            database = db.getDb(basePath/f"{user}/database.json")
+            database = db.getDb((f"{user}/database.json"))
             for data in database.getAll():
+                workDir = Path(user)/str(data["id"])
                 if data["status"] == "started":
-                    if createWorkDir(data["id"], basePath):
-                        data["status"] == "Created workDir"
-                        workDir = Path(basePath)/str(data["id"])
-                    else:
+                    try:
+                        workDir.mkdir(parents=True, exist_ok=True)
+                        database.updateById(data["id"],{"status":"Created workDir"})
+                        data = database.find(data["id"])
+                        print("created workdir")
+                    except:
                         data["status"] == "Failed"
+                        database.updateById(data["id"],{"status":"Failed"})
+                        data = database.find(data["id"])
                 if data["status"] == "Created workDir":
                     if generate_config(data["params"], workDir):
-                        data["status"] == "Generated config"
+                        database.updateById(data["id"],{"status":"Generated config"})
+                        data = database.find(data["id"])
+                        print("Generated config")
                     else:
-                        data["status"] == "Failed"
+                        print("Failed generating config")
+                        database.updateById(data["id"],{"status":"Failed"})
+                        data = database.find(data["id"])
                 if data["status"] == "Generated config":
-                    if generate_mesh_box(data["params"], workDir):
-                        data["status"] == "Generated mesh"
+                    if generate_mesh_box(data["params"], str(workDir.resolve())):
+                        database.updateById(data["id"],{"status":"Generated mesh"})
+                        data = database.find(data["id"])
+                        print("Generated mesh")
                     else:
                         data["status"] == "Failed"
+                        database.updateById(data["id"],{"status":"Failed"})
+                        data = database.find(data["id"])
                 if data["status"] == "Generated mesh":
                     try:
                         run_mofem(data["params"], workDir)
-                        data["status"] == "Simulated"
+                        database.updateById(data["id"],{"status":"Simulated"})
+                        data = database.find(data["id"])
                     except:
                         data["status"] == "Failed"
+                        database.updateById(data["id"],{"status":"Failed"})
+                        data = database.find(data["id"])
                 if data["status"] == "Simulated":
                     # convert results to vtk
                     try:
                         convert(files=list(workDir.glob('out*')), params=data["params"], path=workDir, log_file=Path(workDir)/"convert.log")
-                        data["status"] == "Finished"
+                        database.updateById(data["id"],{"status":"Finished"})
                     except:
-                        data["status"] == "Failed"
-    
+                        database.updateById(data["id"],{"status":"Failed"})
+                        data = database.find(data["id"])
+        print("waiting for 10 s")
+        sleep(10)

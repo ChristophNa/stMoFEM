ea10d71d4e73b8251a80293e4c0e31060eb5d9fa
naumannc <christoph.naumann1985@gmail.com>
2024-06-30 20:52:22 +0200

    debugged

--- MoFEM/runSimulation.py
+++ MoFEM/runSimulation.py
@@ -1,9 +1,15 @@
+import shutil
 import pysondb as db
 from pathlib import Path
 import subprocess
 import gmsh
 from convert import convert
 from time import sleep
+import logging
+import sys
+sys.path.append("um_view/bin")
+
+logging.basicConfig(filename='logSimulation.txt', level=logging.DEBUG)
 
 # generation of a config file - what attributes should the blocksets have
 def generate_config(params, path):
@@ -15,16 +21,16 @@ def generate_config(params, path):
             # Use a for loop to write each line of data to the file
             for line in data:
                 f.write(line + '\n')
-                # print the data as it is written to the file
-                print(line)
+                # logging.debug the data as it is written to the file
+                logging.debug(line)
             # FORCE_1 boundary condition (change in params bellow)
             data = ['[SET_ATTR_FORCE_1]', 'number_of_attributes=3', 'user1='+str(params["force_x"]), 
                     'user2='+str(params["force_y"]), 'user3='+str(params["force_z"])]
             # Use a for loop to write each line of data to the file
             for line in data:
                 f.write(line + '\n')
-                # print the data as it is written to the file
-                print(line)
+                # logging.debug the data as it is written to the file
+                logging.debug(line)
         return True
     except:
         return False
@@ -33,6 +39,7 @@ def generate_config(params, path):
 # gmsh creation of a 3D beam + visualisation of it
 def generate_mesh_box(params, path):
     try:
+        binPath = Path("um_view/bin/")
         if (params["length_x"]+params["length_y"]+params["length_z"])/params["element_size"] > 600:
             raise Exception("Not more than 200 elements per side should be used in this tutorial")
         
@@ -77,12 +84,13 @@ def generate_mesh_box(params, path):
         
         # translate .med file to a format readable by MoFEM and assign values to physical groups
         h5m_file=Path(path)/"mesh.h5m"
-        subprocess.run(["read_med", "-med_file", Path(path)/"mesh.med", "output_file", Path(path)/"mesh.h5m", 
+        subprocess.run([binPath/"read_med", "-med_file", Path(path)/"mesh.med", "-output_file", Path(path)/"mesh.h5m", 
                     "-meshsets_config", Path(path)/"config.cfg", "-log_sl", "error"])
         vtk_file= Path(path)/"mesh.vtk"
-        subprocess.run(["mbconvert", h5m_file, vtk_file])
+        subprocess.run([binPath/"mbconvert", h5m_file, vtk_file])
         return True
-    except:
+    except Exception as e:
+        logging.debug(e)
         return False
     # TODO: visualise the mesh
     # if params.show_mesh:
@@ -105,41 +113,72 @@ def parse_log_file(log_file):
         for line in lf:
             line = line.strip()
             if "nb global dofs" in line:
-                res.elem_num = int(line.split()[13])
+                res["elem_num"] = int(line.split()[13])
             if "norm_u: " in line:
-                res.norm_u = float(line.split()[4])
+                res["norm_u"] = float(line.split()[4])
             if "norm_piola: " in line:
-                res.norm_piola = float(line.split()[4])
+                res["norm_piola"] = float(line.split()[4])
             if "norm_u_h1: " in line:
-                res.norm_u_h1 = float(line.split()[4])
+                res["norm_u_h1"] = float(line.split()[4])
             if "norm_error_u_l2: " in line:
-                res.norm_error_u_l2 = float(line.split()[4])
+                res["norm_error_u_l2"] = float(line.split()[4])
     return res
 
 # running MoFEM executable
-def run_mofem(exe, params, path):
+def run_mofem(params, path):
 
     # define name of the mesh file
-    part_file = Path(path)/"mesh" + f'_{params["nproc"]}p.h5m'
+    part_file = Path(path)/("mesh" + f'_{params["nproc"]}p.h5m')
     log_file = Path(path)/"MoFEM.log"
+    binPath = Path("um_view/bin/")
     
     # partition the mesh
-    subprocess.run(["mofem_part", "-my_file", Path(path)/"mesh" + ".h5m", "-my_nparts", str(params["nproc"]), "-output_file", part_file])
+    subprocess.run([binPath/"mofem_part", "-my_file", Path(path)/"mesh.h5m", "-my_nparts", str(params["nproc"]), "-output_file", part_file])
     
     # choose MoFEM executable
-    um_view = "$HOME/um_view"
-    exes = {"Mixed": um_view + "/*eshelbian_*/ep", "Standard": um_view + "/tutorials/vec-2/nonlinear_elastic"}
-    subprocess.run(["export", "OMPI_MCA_btl_vader_single_copy_mechanism=none", "&&",
-                    "nice", "-n", "10", "mpirun", "--oversubscribe", "--allow-run-as-root",
-                    "-np", params["nproc"], exes[params["formulation"]],
-                    "-file_name", part_file, "-my_file", part_file,
-                    "-order", params["order"],
-                    "-space_order", params["order"] + 1,
-                    "-hencky_young_modulus", params["young_modulus"],
-                    "-hencky_poisson_ratio", params["poisson_ratio"],
-                    "-rotations", params["rotations"],
-                    "-stretches", params["stretches"],
-                    "2>&1", "|", "tee", log_file])
+    um_view = "/mofem_install/um_view"
+    exes = {"Mixed": um_view + "/eshelbian_plasticty/ep", "Standard": um_view + "/tutorials/vec-2/nonlinear_elastic"}
+    # copy petsc parameter file to current directory
+    # shutil.copyfile(Path(exes[params["formulation"]]).parent/"param_file.petsc", Path(path)/"param_file.petsc")
+    shutil.copyfile(Path("app/MoFEM")/"param_file.petsc", Path(path)/"param_file.petsc")
+    with open(Path(path)/"start.sh", "w") as f:
+        f.write("#!/bin/bash\n")
+        f.write("export OMPI_MCA_btl_vader_single_copy_mechanism=none\n")
+        
+        # f.write(" ".join(["nice", "-n", "10", "mpirun", "--oversubscribe", "--allow-run-as-root",
+        #             "-np", str(params["nproc"]), exes[params["formulation"]],
+        #             "-file_name", str(part_file.resolve()), "-my_file", str(part_file.resolve()),
+        #             "-order", str(params["order"]),
+        #             "-space_order", str(params["order"] + 1),
+        #             "-hencky_young_modulus", str(params["young_modulus"]),
+        #             "-hencky_poisson_ratio", str(params["poisson_ratio"]),
+        #             "-rotations", str(params["rotations"]),
+        #             "-stretches", str(params["stretches"]),
+        #             "2>&1", "|", "tee", str(log_file.resolve())]))
+        f.write("cd " + str(Path(path).resolve()) + "\n")
+        f.write(" ".join(["nice", "-n", "10", "mpirun", "--oversubscribe", "--allow-run-as-root",
+                    "-np", str(params["nproc"]), exes[params["formulation"]],
+                    "-file_name", "mesh" + f'_{params["nproc"]}p.h5m', "-my_file", "mesh" + f'_{params["nproc"]}p.h5m',
+                    "-order", str(params["order"]),
+                    "-space_order", str(params["order"] + 1),
+                    "-hencky_young_modulus", str(params["young_modulus"]),
+                    "-hencky_poisson_ratio", str(params["poisson_ratio"]),
+                    "-rotations", str(params["rotations"]),
+                    "-stretches", str(params["stretches"]),
+                    "2>&1", "|", "tee", "MoFEM.log"]))
+    subprocess.run(["chmod", "+x", Path(path)/"start.sh"])
+    subprocess.run([Path(path)/"start.sh"])
+    # subprocess.run(["export", "OMPI_MCA_btl_vader_single_copy_mechanism=none", "&&",
+    #                 "nice", "-n", "10", "mpirun", "--oversubscribe", "--allow-run-as-root",
+    #                 "-np", str(params["nproc"]), exes[params["formulation"]],
+    #                 "-file_name", part_file, "-my_file", part_file,
+    #                 "-order", str(params["order"]),
+    #                 "-space_order", str(params["order"] + 1),
+    #                 "-hencky_young_modulus", str(params["young_modulus"]),
+    #                 "-hencky_poisson_ratio", str(params["poisson_ratio"]),
+    #                 "-rotations", str(params["rotations"]),
+    #                 "-stretches", str(params["stretches"]),
+    #                 "2>&1", "|", "tee", log_file])
 
 
     # return results found by parse_log_file
@@ -154,7 +193,7 @@ if __name__=="__main__":
     
     while True:
         users = [item for item in basePath.iterdir() if item.is_dir()]
-        print(users)
+        logging.debug(users)
         for user in users:
             database = db.getDb((f"{user}/database.json"))
             for data in database.getAll():
@@ -164,8 +203,9 @@ if __name__=="__main__":
                         workDir.mkdir(parents=True, exist_ok=True)
                         database.updateById(data["id"],{"status":"Created workDir"})
                         data = database.getById(data["id"])
-                        print("created workdir")
-                    except:
+                        logging.debug("created workdir")
+                    except Exception as e:
+                        logging.debug(e)
                         data["status"] == "Failed"
                         database.updateById(data["id"],{"status":"Failed"})
                         data = database.getById(data["id"])
@@ -173,16 +213,16 @@ if __name__=="__main__":
                     if generate_config(data["params"], workDir):
                         database.updateById(data["id"],{"status":"Generated config"})
                         data = database.getById(data["id"])
-                        print("Generated config")
+                        logging.debug("Generated config")
                     else:
-                        print("Failed generating config")
+                        logging.debug("Failed generating config")
                         database.updateById(data["id"],{"status":"Failed"})
                         data = database.getById(data["id"])
                 if data["status"] == "Generated config":
                     if generate_mesh_box(data["params"], str(workDir.resolve())):
                         database.updateById(data["id"],{"status":"Generated mesh"})
                         data = database.getById(data["id"])
-                        print("Generated mesh")
+                        logging.debug("Generated mesh")
                     else:
                         data["status"] == "Failed"
                         database.updateById(data["id"],{"status":"Failed"})
@@ -192,17 +232,22 @@ if __name__=="__main__":
                         run_mofem(data["params"], workDir)
                         database.updateById(data["id"],{"status":"Simulated"})
                         data = database.getById(data["id"])
-                    except:
+                        logging.debug("Simulated")
+                    except Exception as e:
+                        logging.debug(e)
                         data["status"] == "Failed"
                         database.updateById(data["id"],{"status":"Failed"})
                         data = database.getById(data["id"])
                 if data["status"] == "Simulated":
                     # convert results to vtk
                     try:
-                        convert(files=list(workDir.glob('out*')), params=data["params"], path=workDir, log_file=Path(workDir)/"convert.log")
+                        convert(files=list(workDir.glob('out*')), np=data["params"]["nproc"], log_file=Path(workDir)/"convert.log")
+                        logging.debug("Finished")
                         database.updateById(data["id"],{"status":"Finished"})
-                    except:
+                        data = database.getById(data["id"])
+                    except Exception as e:
+                        logging.debug(e)
                         database.updateById(data["id"],{"status":"Failed"})
                         data = database.getById(data["id"])
-        print("waiting for 10 s")
+        logging.debug("waiting for 10 s")
         sleep(10)
